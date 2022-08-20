426e5bedc7bb10c900319493c8dcb325537569a8
imanefi <109866162+imanefi@users.noreply.github.com>
2022-08-20 06:35:04 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function BMI(weight,height) {
+    let result = weight / (height * height) ;
+    return result;
+}
 
+console.log(BMI(80 , 1.6));
+
+function status(bmi) {
+    if (bmi < 18.5) {
+        return("لديك نقص في الوزن") 
+    } else if (bmi >=18.5 && bmi < 25) {
+        return("وزنك صحي")
+    } else if (bmi >= 25) {
+        return("لديك زيادة في الوزن")
+    }
+}
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value
+  
+    let bmi = BMI(weight , height);
+    let desc = Status(bmi);
+   
+    let div = document.getElementById("result")
+    div.innerText = bmi + " == " + desc
+}
+
+
+
+console.log(BMI(50 , 1.5 ));
+console.log(BMI())
